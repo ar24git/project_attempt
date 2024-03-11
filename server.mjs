1b7ebe377723070d898265d31cb65b4b050ec848
ar24git <arthurantonuyk@gmail.com>
2024-03-11 15:26:11 +0200

    style connected

--- server.mjs
+++ server.mjs
@@ -9,6 +9,14 @@ app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
+app.get("/page1", (req, res) => {
+  res.render("page1.ejs");
+});
+
+app.get("/page2", (req, res) => {
+  res.render("page2.ejs");
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
