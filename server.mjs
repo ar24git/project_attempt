c12b5f7814c0c2858bc02f92bf35b6771587d959
ar24git <arthurantonuyk@gmail.com>
2024-03-17 22:48:31 +0200

    set up routes

--- server.mjs
+++ server.mjs
@@ -39,6 +39,69 @@ app.get("/page2", (req, res) => {
   res.render("page2.ejs");
 });
 
+// anakoinwseis
+app.get("/anakoinwseis", (req, res) => {
+  res.render("./anakoinwseis/anakoinwseis.ejs");
+});
+
+// agwnes
+app.get("/programma", (req, res) => {
+  res.render("./agwnes/programma.ejs");
+});
+
+app.get("/apotelesmata", (req, res) => {
+  res.render("./agwnes/apotelesmata.ejs");
+});
+
+app.get("/vathmologies", (req, res) => {
+  res.render("./agwnes/vathmologies.ejs");
+});
+
+app.get("/poines", (req, res) => {
+  res.render("./agwnes/poines.ejs");
+});
+
+app.get("/diorganwseis", (req, res) => {
+  res.render("./agwnes/diorganwseis.ejs");
+});
+
+app.get("/statistika", (req, res) => {
+  res.render("./agwnes/statistika.ejs");
+});
+
+// genika
+app.get("/ghpeda", (req, res) => {
+  res.render("./genika/ghpeda.ejs");
+});
+
+app.get("/podosfairistes", (req, res) => {
+  res.render("./genika/podosfairistes.ejs");
+});
+
+app.get("/swmateia", (req, res) => {
+  res.render("./genika/swmateia.ejs");
+});
+
+app.get("/entypa", (req, res) => {
+  res.render("./genika/entypa.ejs");
+});
+
+app.get("/deltio", (req, res) => {
+  res.render("./genika/deltio.ejs");
+});
+
+app.get("/diaithsia", (req, res) => {
+  res.render("./genika/diaithsia.ejs");
+});
+
+app.get("/kanonismoi", (req, res) => {
+  res.render("./genika/kanonismoi.ejs");
+});
+
+app.get("/epitropes", (req, res) => {
+  res.render("./genika/epitropes.ejs");
+});
+
 const dataToInsert = {
   password: 'Jwerw',
   email: 'jowewh22n@example.com',
