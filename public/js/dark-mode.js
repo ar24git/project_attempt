0667103fd77f7240690af62895faa57466bc273d
ar24git <arthurantonuyk@gmail.com>
2024-04-22 19:18:00 +0300

    v2 dark mode

--- public/js/dark-mode.js
+++ public/js/dark-mode.js
@@ -1,14 +1,16 @@
+// both files must be linked
+//dark mode to body
+//and dark mode onload to head
+
 const toggle = document.querySelector('#dark-mode-toggle');
 
-// Check local storage for dark mode preference
-let isDarkModeEnabled = localStorage.getItem('dark-mode') === 'true';
+// // Check local storage for dark mode preference
+// let isDarkModeEnabled = localStorage.getItem('dark-mode') === 'true';             is already declared in the head
 if (isDarkModeEnabled) {
   // Enable dark mode if preference is stored
   setDarkMode(true);
-} else {
-  // Otherwise, ensure light mode is set
-  setDarkMode(false);
 }
+
 
 // Function to set dark mode
 function setDarkMode(enabled) {
