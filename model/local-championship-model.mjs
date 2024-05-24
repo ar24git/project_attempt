cdd99c6af889fefd262a9b9ef0ea51c6d9339eee
ar24git <arthurantonuyk@gmail.com>
2024-05-24 22:29:35 +0300

    get all matches implementation

--- model/local-championship-model.mjs
+++ model/local-championship-model.mjs
@@ -18,31 +18,66 @@ try{
     throw Error('Failure connecting to database');
 }
 
-  export let getAllMatches = async() =>{
-    const match = {
-        name: 'Match 1',
-        pitch: 'Pitch A',
-        date: '2024-05-21',
-        time: '14:00',
-        referee: 'Referee 1',
-        result: '2-1'
-      };
-    
-    const matches = [];
-      matches.push(match);
-      matches.push(match);
-      matches.push(match);
-    return matches;
-  }
-
 //will populate database for testing
 //run resetDatabase in postgre
 //and set insertData to true in order to populate db
 import * as populate from "./populateDatabase.mjs"
-const insertData = true;
+const insertData = false;
 if (insertData) {
   populate.insertData();
 }
+
+
+  export let getAllMatches = async() =>{
+    let matches;
+    const query = `
+      SELECT 
+        m.Category,
+        m.Season,
+        m.Association,
+        m.MatchResult,
+        p.Time,
+        p.Date,
+        f.Name AS FieldName,
+        c1.Name AS HomeClubName,
+        c2.Name AS AwayClubName,
+        r.Id AS RefereeId,
+        per.Name AS RefereeName
+        FROM 
+        Match m
+    JOIN 
+        Play p ON m.MatchId = p.MatchId
+    JOIN 
+        Field f ON p.FieldName = f.Name
+    JOIN 
+        Club c1 ON p.IdHome = c1.Name
+    JOIN 
+        Club c2 ON p.IdAway = c2.Name
+    JOIN 
+        Judges j ON m.MatchId = j.MatchId
+    JOIN 
+        Referee r ON j.RefereeId = r.Id
+    JOIN 
+        Person per ON r.Id = per.Id
+    `;
+    matches = await db.query(query);
+    console.log(matches.rows);
+    let formatedMatches = matches.rows.map(match=>{
+      return {
+        name: match.homeclubname+" : "+match.awayclubname,
+        pitch: match.fieldname,
+        date: match.date.toLocaleString('el-GR',{ dateStyle: 'short' }),
+        time: match.time,
+        referee: match.refereename,
+        result: match.matchresult
+      }
+    })
+
+    return formatedMatches;
+  }
+
+
+
 
 
   
